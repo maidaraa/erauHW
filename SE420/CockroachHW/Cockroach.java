c69ffcfa1bca036f60840fa9d13387642b3670b2
Mai <96503712+maidaraa@users.noreply.github.com>
2025-01-23 14:15:47 -0600

    Create Cockroach.java

---
+++ SE420/CockroachHW/Cockroach.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Cockroach {
+    private InputHandler InputHandler = new InputHandler();
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Cockroach roach = new Cockroach();
+        Scanner in = new Scanner(System.in);
+        roach.InputHandler.input(in); 
+    }
+
+   
+
+    
+
+
+
+
+
+}
